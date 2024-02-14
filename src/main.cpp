2b3c26dd46d4517a622bc4475a1dbe81e1ae7eba
namaya <n3amaya@gmail.com>
2024-02-13 21:01:48 -0600

    initial input parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 
 #include <iostream>
 
-int main() {
-  std::cout << "Hello, World!\n";
+#include <argparse/argparse.hpp>
+
+int main(int argc, char *argv[]) {
+  argparse::ArgumentParser program("clippy");
+
+  program.add_argument("library").help("The library to use");
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::runtime_error &err) {
+    std::cout << err.what() << "\n";
+    std::cout << program;
+    return 1;
+  }
+
+  auto library = program.get<std::string>("library");
+
+  std::cout << "Library: " << library << "\n";
+
   return 0;
 }
